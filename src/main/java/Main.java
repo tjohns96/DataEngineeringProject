1bf90afc14caae450f3f2c6a7d593ac3ab8062c8
tjohns96 <tajohnson4499@eagle.fgcu.edu>
2020-02-06 12:27:21 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            Path repairsFile = Paths.get("repairs.csv");
+            if (Files.exists(repairsFile)) {
+                Scanner s = new Scanner(repairsFile);
+                while (s.hasNextLine()) {
+                    String next = s.nextLine();
+                    if (!(next.trim().isEmpty())) {
+                        System.out.println(next);
+                    } else {
+                        continue;
+                    }
+                }
+            } else {
+                System.out.println("This file does not exist.");
+            }
+        } catch (Exception e) {
+            System.out.println("Uh oh! Something has gone wrong :(");
+        }
+    }
+}
+
